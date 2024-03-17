a51fe27bc405b2d1745e3c93281f540efc34b57c
rahdi <adrian.heiden@gmail.com>
2024-03-17 18:24:07 +0100

    disabled 'Go to summary' button, when there is no scoops added - added code & tests

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Options from "./Options";
 import GrandTotal from "./GrandTotal";
 import { Button } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const OrderEntry = ({ handleClick }) => {
+  const { totals } = useOrderDetails();
+
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <GrandTotal />
 
-      <Button variant="primary" size="lg" onClick={handleClick}>
+      <Button
+        variant="primary"
+        size="lg"
+        onClick={handleClick}
+        disabled={totals.scoops === 0}
+      >
         Go to summary
       </Button>
     </div>
--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,11 +1,13 @@
 import { http, HttpResponse } from "msw";
-import { server } from "../../../mocks/server";
+import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
+import { server } from "../../../mocks/server";
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import OrderEntry from "../OrderEntry";
 
 // we can use "test.only" to test only one test (in case of multiple tests in one file)
-test.only("Handles error for scoops and toppings routes", async () => {
+test("Handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
     http.get("http://localhost:3030/scoops", () => {
       return new HttpResponse(null, { status: 500 });
@@ -29,3 +31,30 @@ test.only("Handles error for scoops and toppings routes", async () => {
 
 // we can use "test.skip" to skip one test
 test.skip("my test 2", () => {});
+
+test("Disable 'Go to summary' button, when there is no scoops", async () => {
+  const user = userEvent.setup();
+
+  render(<OrderEntry handleClick={() => {}} />); // <- TS would complain
+
+  // order button should be disabled at start
+  const button = screen.getByRole("button", { name: "Go to summary" });
+  expect(button).toBeDisabled();
+
+  // button should be enabled after adding scoops
+  const chocolateScoop = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  expect(chocolateScoop).toBeInTheDocument();
+  await user.clear(chocolateScoop);
+  await user.type(chocolateScoop, "2");
+  const scoopsTotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsTotal).toHaveTextContent("4.00");
+  expect(button).toBeEnabled();
+
+  // order button should be disabled again, after removing scoops
+  await user.clear(chocolateScoop);
+  await user.type(chocolateScoop, "0");
+  expect(scoopsTotal).toHaveTextContent("0.00");
+  expect(button).toBeDisabled();
+});
